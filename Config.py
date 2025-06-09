23e2f40ab3bcac37efb1cfc855fa7c6f95b5750f
danie3355 <peixoto223366@gmail.com>
2025-06-09 19:42:28 +0100

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,6 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+COINS = ["bitcoin", "dogecoin", "solana", "ethereum"]
+INTERVAL_MINUTES = 15
